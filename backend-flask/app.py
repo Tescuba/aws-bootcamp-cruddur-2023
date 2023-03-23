90399c94d70d8c7050f6cffc27326df63093ae75
Tescuba <selamgualey@gmail.com>
2023-03-23 05:16:35 +0000

    fixing cognito errors 49-20

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -14,6 +14,8 @@ from services.messages import *
 from services.create_message import *
 from services.show_activity import *
 
+from lib.cognito_jwt_token import CognitoJwtToken, extract_access_token, TokenVerifyError
+
 # HoneyComb ------------
 from opentelemetry import trace
 from opentelemetry.instrumentation.flask import FlaskInstrumentor
@@ -66,6 +68,12 @@ trace.set_tracer_provider(provider)
 tracer = trace.get_tracer(__name__)
 
 app = Flask(__name__)
+
+cognito_jwt_token = CognitoJwtToken(
+  user_pool_id=os.getenv("AWS_COGNITO_USER_POOL_ID"),
+  user_pool_client_id=os.getenv("AWS_COGNITO_USER_POOL_CLIENT_ID"),
+  region=os.getenv("AWS_DEFAULT_REGION")
+)
 
 # X-RAY --------------
 XRayMiddleware(app, xray_recorder)
@@ -155,7 +163,18 @@ def data_create_message():
 @app.route("/api/activities/home", methods=['GET'])
 @xray_recorder.capture('activities_home')
 def data_home():
+  access_token = extract_access_token(request.headers)
+  try:
+    claims = cognito_jwt_token.verify(access_token)
+    # authenticated request
+    app.logger.debug("authenticated")
+    app.logger.debug(claims)
+  except TokenVerifyError as e:
+    # unauthenticated request
+    app.logger.debug("unauthenticated")
+
   data = HomeActivities.run()
+
   return data, 200
 
 # @app.route("/api/activities/notifications", methods=['GET'])
