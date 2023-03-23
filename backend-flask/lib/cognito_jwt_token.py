e6a9d0a30959d8e4855074d0e45ab5981a3c7317
Tescuba <selamgualey@gmail.com>
2023-03-23 06:14:30 +0000

    implement backend check for the token

--- backend-flask/lib/cognito_jwt_token.py
+++ backend-flask/lib/cognito_jwt_token.py
@@ -31,6 +31,7 @@ class CognitoJwtToken:
             self.request_client = request_client
         self._load_jwk_keys()
 
+
     def _load_jwk_keys(self):
         keys_url = f"https://cognito-idp.{self.region}.amazonaws.com/{self.user_pool_id}/.well-known/jwks.json"
         try:
