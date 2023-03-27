a45124644320d197bca8a38b53be48eeabc1fa21
Tescuba <selamgualey@gmail.com>
2023-03-27 06:36:32 +0000

    post confirmation lamda

--- aws/json/lambdas/cruddur-post-confirmation.py
+++ aws/json/lambdas/cruddur-post-confirmation.py
@@ -14,7 +14,7 @@ def lambda_handler(event, context):
     try:
         print('entered-try')
         sql = f"""
-            "INSERT INTO users (
+            INSERT INTO public.users (
             display_name,
             email,
             handle,
@@ -25,7 +25,7 @@ def lambda_handler(event, context):
             '{user_email}',
             '{user_handle}',
             '{user_cognito_id}'
-            )"
+            )
         """
         print('SQL Statement ----')
         print(sql)
